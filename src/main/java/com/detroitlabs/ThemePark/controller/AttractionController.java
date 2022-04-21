c3529bc7129413c5ea4b0d00e2cd558a27becaea
Elissa Olivas <elissaolivas@Elissas-MacBook-Pro.local>
2022-04-21 15:39:41 -0600

    Adding content and style to attractions page

--- src/main/java/com/detroitlabs/ThemePark/controller/AttractionController.java
+++ src/main/java/com/detroitlabs/ThemePark/controller/AttractionController.java
@@ -3,12 +3,15 @@ package com.detroitlabs.ThemePark.controller;
 
 
 import com.detroitlabs.ThemePark.data.AttractionRepository;
+import com.detroitlabs.ThemePark.model.Food;
 import com.detroitlabs.ThemePark.model.Lands;
+import com.detroitlabs.ThemePark.model.Restaurants;
 import com.detroitlabs.ThemePark.model.Rides;
 import com.detroitlabs.ThemePark.service.ParkService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.io.IOException;
@@ -28,5 +31,13 @@ public class AttractionController {
         modelMap.put("allLands", attractionRepository.landsList());
         modelMap.put("allRides", attractionRepository.ridesList());
         return "attractions";
+    }
+
+    @RequestMapping("/attraction-details/{name}")
+    public String displayRestaurantMenu(@PathVariable String name, ModelMap modelMap) throws IOException {
+        String lands = attractionRepository.findByName(name);
+        modelMap.put("lands", lands);
+        modelMap.put("allRides", attractionRepository.ridesList());
+        return "attraction-details";
     }
 }
